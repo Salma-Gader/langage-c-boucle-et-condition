35522a0f4a706067284675d9e0e69c56335d2e00
Salma-Gader <salmagader@gmail.com>
2022-09-12 09:18:45 +0100

    realisation de challenge1

---
+++ Challenge 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+    int num;
+	printf ("entrer le nomber\n");
+	scanf ("%d",&num);
+	if (num%2==0) 
+	{
+		printf ("nombre est paire");
+	 } 
+	 else
+	 {
+	 	printf("nombre est impaire");
+	 }
+	return 0;
+}
